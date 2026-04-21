933370090f376255c18e913eadeb73b8b9a61af6
Vasileios Ntoufoudis <info@ntoufoudis.com>
2026-04-21 17:53:58 +0300

    feat: add BookingService with transaction locking and double-booking prevention

--- app/Jobs/SendBookingConfirmation.php
+++ app/Jobs/SendBookingConfirmation.php
@@ -4,22 +4,17 @@ namespace App\Jobs;
 
 use App\Models\Appointment;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Queue\ShouldQueueAfterCommit;
 use Illuminate\Foundation\Queue\Queueable;
 
-class SendBookingConfirmation implements ShouldQueue
+class SendBookingConfirmation implements ShouldQueue, ShouldQueueAfterCommit
 {
     use Queueable;
-
-    /** Dispatch only after the DB transaction commits */
-    public bool $afterCommit = true;
 
     /**
      * Create a new job instance.
      */
-    public function __construct(public readonly Appointment $appointment)
-    {
-        //
-    }
+    public function __construct(public readonly Appointment $appointment) {}
 
     /**
      * Execute the job.
