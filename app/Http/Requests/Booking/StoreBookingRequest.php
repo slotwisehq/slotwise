91802a8a976e7203f200a34000cd9d939880af9f
Vasileios Ntoufoudis <info@ntoufoudis.com>
2026-04-21 19:14:32 +0300

    feat: add StoreBookingRequest form validation

---
+++ app/Http/Requests/Booking/StoreBookingRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Booking;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBookingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'starts_at' => ['required', 'date_format:Y-m-d H:i:s'],
+            'customer_name' => ['required', 'string', 'max:255'],
+            'customer_email' => ['required', 'email', 'max:255'],
+            'customer_phone' => ['nullable', 'string', 'max:50'],
+        ];
+    }
+}
