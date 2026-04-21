d8e11058fdd52041e4683eff7e3c028c79040660
Vasileios Ntoufoudis <info@ntoufoudis.com>
2026-04-21 18:30:49 +0300

    feat: register public booking routes

---
+++ routes/booking.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Booking\BookingController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('book/{tenant:slug}')->name('booking.')->group(function () {
+    Route::get('/', [BookingController::class, 'show'])->name('show');
+    Route::get('/{service}', [BookingController::class, 'selectStaff'])->name('staff');
+    Route::get('/{service}/{staff}', [BookingController::class, 'selectSlot'])->name('slots');
+    Route::get('/{service}/{staff}/book', [BookingController::class, 'showCustomerForm'])->name('form');
+    Route::post('/{service}/{staff}/book', [BookingController::class, 'store'])->name('store');
+    Route::get('/confirmation/{appointment}', [BookingController::class, 'confirmation'])->name('confirmation');
+});
